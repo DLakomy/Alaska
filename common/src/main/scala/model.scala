00d7a8f0d044a02d76a2a3e36532cc383a4e7ea2
Dawid Łakomy <dawid.lakomy1@gmail.com>
2023-01-06 16:04:39 +0100

    Replace trait with abstract class

--- common/src/main/scala/model.scala
+++ common/src/main/scala/model.scala
@@ -3,7 +3,7 @@ package alaska.model
 type ParseResult = Either[String, List[Valid.Entry]]
 
 object Valid {
-  sealed trait Entry
+  sealed abstract class Entry
   case class Num(recNum: Integer, fieldNum: Integer, value: Integer) extends Entry
   case class Text(recNum: Integer, fieldNum: Integer, value: String) extends Entry
 }
