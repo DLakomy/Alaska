89bf6bfa08a8d1097916fb9d74c863d9804da8f1
Dawid Łakomy <dawid.lakomy1@gmail.com>
2023-01-14 14:08:50 +0100

    Implement simple version

--- common/src/main/scala/typeclasses.scala
+++ common/src/main/scala/typeclasses.scala
@@ -16,6 +16,15 @@ given CsvStringSerializer[Valid.Text] with {
     List(input.recNum, input.fieldNum, s"\"${input.value}\"").mkString(",")
 }
 
+// TODO compiler couldn't figure this out
+// CsvStringSerializer.serialize(entry) fails if entry is an instance of Valid.Entry
+// this code smells a bit; at lest non exhaustive match is caught
+given CsvStringSerializer[Valid.Entry] with {
+  def serialize(input: Valid.Entry): String = input match
+    case n: Valid.Num => CsvStringSerializer.serialize(n)
+    case t: Valid.Text => CsvStringSerializer.serialize(t)
+}
+
 object CsvStringSerializer {
   def apply[A](using c: CsvStringSerializer[A]): CsvStringSerializer[A] = c
   def serialize[A](input: A)(using c: CsvStringSerializer[A]): String = c.serialize(input)
---
+++ simple/src/main/scala/simple.scala
@@ -0,0 +1,42 @@
+package alaska.simple
+
+import alaska.model.Valid
+import alaska.parser.RecordParser
+import alaska.typeclasses.{CsvStringSerializer, given}
+
+import java.io.{BufferedReader, BufferedWriter, FileReader, FileWriter}
+
+// I've never wrote a Scala code this ugly...
+// this one is meant to be simple, as a reference for the second implementation
+
+// TODO header for csv
+def program(source: BufferedReader, numDest: BufferedWriter, txtDest: BufferedWriter, errDest: BufferedWriter): Unit = {
+
+  def handleRecord(record: String): Unit =
+    val parsed = RecordParser.parseRecord(record)
+    parsed match
+      case Left(error) => errDest.write(error+'\n')
+      case Right(entries) => entries.toList.foreach { entry =>
+        val serialized = CsvStringSerializer.serialize(entry)
+        entry match
+          case _: Valid.Num => numDest.write(serialized+'\n')
+          case _: Valid.Text => txtDest.write(serialized+'\n')
+      }
+
+  var line = source.readLine()
+  val accumulator: StringBuilder = new StringBuilder
+  while (line != null) {
+    if (line matches "^Record.*") accumulator.append(line)+='\n'
+    else if (line == "%") {
+      accumulator.append(line)+='\n'
+      handleRecord(accumulator.toString())
+      accumulator.clear()
+    }
+    // ...isEmpty handles filtering garbage between records
+    else if (accumulator.nonEmpty) accumulator.append(line)+='\n'
+    line = source.readLine()
+  }
+  numDest.flush()
+  txtDest.flush()
+  errDest.flush()
+}
