016981575d7c3d3acaeb25deabf7e92f383ae10f
Dawid Łakomy <dawid.lakomy1@gmail.com>
2023-01-21 22:00:37 +0100

    Fix serializer tests

--- common/src/test/scala/CsvStringSerializerSpec.scala
+++ common/src/test/scala/CsvStringSerializerSpec.scala
@@ -6,14 +6,14 @@ class CsvStringSerializerSpec extends munit.FunSuite {
   test("Serializing number row") {
     val input = Valid.Num(1,23,66)
     val obtained = CsvStringSerializer.serialize(input)
-    val expected = "1,23,66"
+    val expected = "1,23,66\n"
     assertEquals(obtained, expected)
   }
 
   test("Serializing text row") {
     val input = Valid.Text(1, 23, "66")
     val obtained = CsvStringSerializer.serialize(input)
-    val expected = "1,23,\"66\""
+    val expected = "1,23,\"66\"\n"
     assertEquals(obtained, expected)
   }
 }
